d2146870be842e7fd0276dc90f8ff7c50fc1a956
Damian <damianjjoniec@gmail.com>
2019-09-07 23:55:06 +0100

    front end and back end connected

--- src/App.js
+++ src/App.js
@@ -1,104 +1,113 @@
-import React, {Component} from 'react';
-import 'tachyons';
-import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
+import React, { Component } from 'react'
+import 'tachyons'
+import Particles from 'react-particles-js'
+import Clarifai from 'clarifai'
 
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import SignInForm from './components/SignInForm/SignInForm';
-import Register from './components/Register/Register';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import './App.css'
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import SignInForm from './components/SignInForm/SignInForm'
+import Register from './components/Register/Register'
 
 const app = new Clarifai.App({
-    apiKey: '13473d242ca84681b29763a85fe850a1'
-});
+  apiKey: '13473d242ca84681b29763a85fe850a1'
+})
 
 const particlesOptions = {
-    particles: {
-        number: {
-            value: 350,
-            density: {
-                enable: true,
-                value_area: 2000
-            }
-        }
+  particles: {
+    number: {
+      value: 350,
+      density: {
+        enable: true,
+        value_area: 2000
+      }
     }
+  }
 }
 
 class App extends Component {
+  state = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false
+  }
 
-    state = {
-        input: '',
-        imageUrl: '',
-        box: {},
-        route: 'signin'
+  calculateFaceLocation = data => {
+    const face = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+    console.log(width, height)
+    return {
+      leftCol: face.left_col * width,
+      topRow: face.top_row * height,
+      rightCol: width - face.right_col * width,
+      bottomRow: height - face.bottom_row * height
     }
+  }
 
-    calculateFaceLocation = data => {
-        const face = data.outputs[0].data.regions[0].region_info.bounding_box;
-        const image = document.getElementById('inputImage');
-        const width = Number(image.width);
-        const height = Number(image.height);
-        console.log(width, height);
-        return {
-            leftCol: face.left_col * width,
-            topRow: face.top_row * height,
-            rightCol: width - (face.right_col * width),
-            bottomRow: height - (face.bottom_row * height)
-        }
-    }
+  displayBox = box => {
+    this.setState({ box: box })
+  }
 
-    displayBox = box => {
-        this.setState({box: box})
-    }
+  handleInputChange = event => {
+    this.setState({ input: event.target.value })
+  }
 
-    handleInputChange = event => {
-        this.setState({input: event.target.value});
-    }
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
 
-    onButtonSubmit = () => {
-        this.setState({imageUrl: this.state.input})
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        // URL
+        this.state.input
+      )
+      .then(response => {
+        // do something with responseconsole.log(response);
+        this.displayBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err))
+  }
 
-        app.models
-            .predict(
-                Clarifai.FACE_DETECT_MODEL,
-                // URL
-                this.state.input
-            )
-            .then(response => {
-                // do something with responseconsole.log(response);
-                this.displayBox(this.calculateFaceLocation(response))
-            })
-            .catch(err => console.log(err))
-    }
+  handleRouteChange = route => {
+    this.setState({ route: route })
+  }
 
-    handleRouteChange = (route) => {
-        this.setState({route: route})
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <Particles className='particles'
-                           params={particlesOptions}
-                />
-                <Navigation isSigned={this.state.route === 'signup'} onRouteChange={this.handleRouteChange} />
-                {this.state.route === 'signup' ? <div>
-                    <Logo/>
-                    <Rank/>
-                    <ImageLinkForm
-                        handleInputChange={this.handleInputChange}
-                        onButtonSubmit={this.onButtonSubmit}/>
-                    <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
-                </div>  : (this.state.route === 'signin' ? <SignInForm onRouteChange={this.handleRouteChange}/> :
-                <Register onRouteChange={this.handleRouteChange} />)
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='App'>
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation
+          isSigned={this.state.route === 'signedup'}
+          onRouteChange={this.handleRouteChange}
+        />
+        {this.state.route === 'signedup' ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              handleInputChange={this.handleInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        ) : this.state.route === 'signin' ? (
+          <SignInForm onRouteChange={this.handleRouteChange} />
+        ) : (
+          <Register onRouteChange={this.handleRouteChange} />
+        )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,34 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-const SignInForm = (props) => {
-    return (
-        <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-            <main className="pa4 black-80">
-                <form className="measure">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f1 fw6 ph0 mh0" >Sign In</legend>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                                   type="email" name="email-address" id="email-address"/>
-                        </div>
-                        <div className="mv3">
-                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                                   type="password" name="password" id="password"/>
-                        </div>
-                    </fieldset>
-                    <div className="">
-                        <input onClick={() => props.onRouteChange('signup')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                               type="submit" value="Sign up"/>
-                    </div>
-                    <div className="lh-copy mt3">
-                        <p onClick={() => props.onRouteChange('register')} className="f5 link dim black db pointer">Register</p>
-                    </div>
-                </form>
-            </main>
-        </article>
-    );
-};
+const SignInForm = props => {
+  const [signInEmail, setSignInEmail] = useState('')
+  const [mypassword, setPassword] = useState('')
 
-export default SignInForm;
+  const onEmailChange = ev => {
+    setSignInEmail(ev.target.value)
+    console.log(signInEmail)
+  }
+
+  const onPasswordChange = ev => {
+    setPassword(ev.target.value)
+    console.log(mypassword)
+  }
+
+  const onSubmitSignIn = ev => {
+    ev.preventDefault()
+    fetch('http://localhost:3000/signin', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: signInEmail,
+        password: mypassword
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        if (data === 'success') {
+          props.onRouteChange('signedup')
+        }
+      })
+  }
+
+  return (
+    <article className='br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
+      <main className='pa4 black-80'>
+        <form className='measure'>
+          <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
+            <legend className='f1 fw6 ph0 mh0'>Sign In</legend>
+            <div className='mt3'>
+              <label className='db fw6 lh-copy f6' htmlFor='email-address'>
+                Email
+              </label>
+              <input
+                className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                type='email'
+                name='email-address'
+                id='email-address'
+                onChange={onEmailChange}
+              />
+            </div>
+            <div className='mv3'>
+              <label className='db fw6 lh-copy f6' htmlFor='password'>
+                Password
+              </label>
+              <input
+                className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                type='password'
+                name='password'
+                id='password'
+                onChange={onPasswordChange}
+              />
+            </div>
+          </fieldset>
+          <div className=''>
+            <input
+              onClick={onSubmitSignIn}
+              className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'
+              type='submit'
+              value='Sign in'
+            />
+          </div>
+          <div className='lh-copy mt3'>
+            <p
+              onClick={() => props.onRouteChange('register')}
+              className='f5 link dim black db pointer'
+            >
+              Register
+            </p>
+          </div>
+        </form>
+      </main>
+    </article>
+  )
+}
+
+export default SignInForm
